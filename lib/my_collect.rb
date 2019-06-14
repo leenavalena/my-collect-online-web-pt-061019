0fdee62c19d6389c3f94268286c854cd6164e91c
Valena Del Valle <valenadelvalle4@gmail.com>
2019-06-14 21:45:22 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,4 +2,6 @@ collection = [ 'ruby', 'javascript', 'python', 'objective-c' ]
 
 empty_array = [ ]
   counter = 0
-def my_collect(empty_array) do 
+def my_collect(empty_array) do |lang|
+  lang.upcase
+end
